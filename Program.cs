adb0bffc48c523e4ac6d0307c8569c985d300bbe
Vimslpb Obsm <global0908@mail.ru>
2023-08-17 23:22:23 +0300

    List<Hotel> => SQLIte database update.

--- Program.cs
+++ Program.cs
@@ -1,34 +1,68 @@
-using System.Security.Cryptography.X509Certificates;
-using Microsoft.AspNetCore.Http.HttpResults;
 
 //httprepl
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<HotelDb>(options=>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("Default"));
+});
+
 var app = builder.Build();
 
-var hotels = new List<Hotel>();
-
-app.MapGet("/hotels", () => hotels);
-app.MapGet("/hotels/{id}", (int id) => hotels[id]);
-
-app.MapPost("/hotels", (Hotel hotel) => hotels.Add(hotel));
-
-app.MapPut("/hotels", (Hotel hotel) =>
+if(app.Environment.IsDevelopment())
 {
-    var index = hotels.FindIndex(h => h.Id == hotel.Id);
-    if(index < 0) { throw new Exception("Not found"); }
-    hotels[index] = hotel;
+    using var scope = app.Services.CreateScope();
+    var db = scope.ServiceProvider.GetRequiredService<HotelDb>();
+    db.Database.EnsureCreated();
+}
+
+
+app.MapGet("/hotels", async (HotelDb db) => await db.Hotels.ToListAsync());
+app.MapGet("/hotels/{id}", async (HotelDb db,int id) => 
+await db.Hotels.FirstOrDefaultAsync(h=> h.Id == id) is Hotel hotel?
+Results.Ok(hotel):
+Results.NotFound()
+);
+
+app.MapPost("/hotels", async ([FromServices]HotelDb db, [FromBody]Hotel hotel, HttpResponse response) =>
+{ 
+    await db.AddAsync(hotel);
+    await db.SaveChangesAsync();
+    return Results.Created($"/hotels/{hotel.Id}", hotel);
 });
 
-app.MapDelete("/hotels/{id}", (int id) =>
+app.MapPut("/hotels", async (HotelDb db, [FromBody]Hotel hotel) =>
 {
-    var index = hotels.FindIndex(h => h.Id == id);
-    if(index < 0) { throw new Exception("Not found"); }
-    hotels.Remove(hotels[index]);
+    var hotelFromDb = await db.Hotels.FindAsync(new int[] { hotel.Id });
+    if (hotelFromDb == null) return Results.NotFound();
+
+    hotelFromDb.Name = hotel.Name;
+    hotelFromDb.CostPerNight=hotel.CostPerNight;
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+}
+);
+app.MapDelete("/hotels/{id}", async (HotelDb db, int id) => 
+{
+    var hotelFromDb = await db.Hotels.FirstOrDefaultAsync(h => h.Id == id);
+    if (hotelFromDb == null) return Results.NotFound();
+    if(hotelFromDb.Id != id) return Results.Conflict(new Exception("Ids are different"));
+    db.Hotels.Remove(hotelFromDb);
+    await db.SaveChangesAsync();
+    return Results.NoContent();
 });
+
+//app.UseHttpsRedirection();
 
 app.Run();
 
+class HotelDb: DbContext
+{
+    public HotelDb(DbContextOptions options):base(options){}
+    public DbSet<Hotel> Hotels{get;set;}
+}
+
+// Model hotel class
 class Hotel
 {
     public int Id {get;set;}

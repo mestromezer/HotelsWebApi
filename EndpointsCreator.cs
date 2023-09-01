568d520cfdd2f77dae1e7624e476d69b652e4901
mestromezer <global0908@mail.ru>
2023-09-01 17:47:17 +0300

    Refactoring. Introducing copde convention.

--- EndpointsCreator.cs
+++ EndpointsCreator.cs
@@ -1,21 +1,15 @@
-public static class ApplicationModifier
+public static class EndpointsCreator
 {
     public static void Modify(ref WebApplication app)
     {
-        if(app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-            using var scope = app.Services.CreateScope();
-            var db = scope.ServiceProvider.GetRequiredService<HotelDb>();
-            db.Database.EnsureCreated();
-        }
-
-        app.MapGet("/hotels", async (IHotelRepository repository) => Results.Ok(await repository.GetHotelAsync()))
+        // Get list of hotels.
+        app.MapGet("/hotels", async (IHotelRepository repository) => 
+        Results.Ok(await repository.GetHotelAsync()))
         .Produces<List<Hotel>>(StatusCodes.Status200OK)
         .WithName("Get all hotels")
         .WithTags("Getters");
 
+        // Get exact hotel info.
         app.MapGet("/hotels/{id}", async (int id, IHotelRepository repository) => 
         await repository.GetHotelAsync(id) is Hotel hotel?
         Results.Ok(hotel):
@@ -24,6 +18,7 @@ public static class ApplicationModifier
         .WithName("Gets hotel by id")
         .WithTags("Getters");
 
+        // Get exact hotel info by query.
         app.MapGet("/hotels/search/name/{query}", async (string query, IHotelRepository repository) =>
             await repository.GetHotelsAsync(query) is IEnumerable<Hotel> hotels
                 ? Results.Ok(hotels)
@@ -33,30 +28,38 @@ public static class ApplicationModifier
             .WithName("Search hotels by query")
             .WithTags("Getters").ExcludeFromDescription(); // exclude from swagger
 
+        // Create new hotel and provide it to the database.
         app.MapPost("/hotels", async ([FromBody]Hotel hotel, IHotelRepository repository) =>
         { 
             await repository.InsertHotelAsync(hotel);
             await repository.SaveAsync();
             return Results.Created($"/hotels/{hotel.Id}", hotel);
-        }).Accepts<Hotel>("application/json").Produces<Hotel>(StatusCodes.Status201Created).WithName("Create new hotel")
+        })
+        .Accepts<Hotel>("application/json")
+        .Produces<Hotel>(StatusCodes.Status201Created)
+        .WithName("Create new hotel")
         .WithTags("Creators");
 
+        // Update info about a hotel.
         app.MapPut("/hotels", async ([FromBody]Hotel hotel, IHotelRepository repository) =>
         {
             await repository.UpdateHotelAsync(hotel, hotel.Id);
             await repository.SaveAsync();
             return Results.NoContent();
         }
-        ).Accepts<Hotel>("application/json").WithName("Update hotel's info")
+        )
+        .Accepts<Hotel>("application/json")
+        .WithName("Update hotel's info")
         .WithTags("Updaters");
 
+        // Delete a hotel from the database.
         app.MapDelete("/hotels/{id}", async (int id, IHotelRepository repository) => 
         {
             await repository.DeleteHotelAsync(id);
             await repository.SaveAsync();
             return Results.NoContent();
-        }).WithName("Delete hotel").WithTags("Deleters");
-
-        app.UseHttpsRedirection();
+        })
+        .WithName("Delete hotel")
+        .WithTags("Deleters");
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
+// Adding services to pipeline.
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<HotelDb>(options=>
@@ -8,16 +9,21 @@ builder.Services.AddDbContext<HotelDb>(options=>
 });
 builder.Services.AddScoped<IHotelRepository,HotelRepository>();
 
+// Configuring API.
 var app = builder.Build();
 app.UseSwaggerUI(options =>
 {
     options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
     options.RoutePrefix = string.Empty;
 });
+app.UseSwagger();
+//app.UseSwaggerUI();
+/*using var scope = app.Services.CreateScope();
+var db = scope.ServiceProvider.GetRequiredService<HotelDb>();
+db.Database.EnsureCreated();*/
+app.UseHttpsRedirection();
+EndpointsCreator.Modify(ref app);
 
-ApplicationModifier.Modify(ref app);
-
+// Run the API.
 app.Run();
-
-// connect swagger:  http://localhost:{port}/swagger/index.html
 
